0dff18c88e2a78877e588c0e3013af481bfc7280
MauriAlcalde <maurialcalde2020@gmail.com>
2021-02-15 08:41:56 -0300

    section de la home creada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
+import Home from './components/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "./styles.css"
 
 function App() {
   return (
-    <h1>Comienzo de app</h1>
+    <> 
+    <BrowserRouter>
+       <Header/>
+       <Switch>
+       <Route exact path='/' component = {Home} />
+       </Switch>
+       <Footer/>
+    </BrowserRouter>
+    </> 
   );
 }
 
---
+++ frontend/src/components/SecondSection.js
@@ -0,0 +1,41 @@
+const SecondSection = ()=>{
+    return (
+        <>
+        <div className="secondSection">
+            <div className="upBoxSecond">
+                <div className="firstUp">
+                    <p>Ver tu historia...</p>
+                </div>
+                <div className="secondUp">
+                    <div className='verContainer'>
+                        <i class="fas fa-tv"></i>
+                        <p>Consigue que tu historia se produzca como una película</p>
+                    </div>
+                    <div className='verContainer'>
+                        <i class="fas fa-film"></i>
+                        <p>Consigue que tu historia se adapte como Serie de Televisión</p>
+                    </div>
+                    <div className='verContainer'>
+                        <i class="fas fa-book"></i>
+                        <p>Conviértete en un autor publicado</p>
+                    </div>
+                </div>
+            </div>
+            <div className='downBoxSecond'>
+                <div className="imgBoxSecond"></div>
+                <div className='textRightSecondBox'>
+                    <p className='firstTextRightSecond'>Tu historia original puede ser un nuevo y grandioso éxito</p>
+                    <p className='secondTextRightSecond'>Entre Lineas Studios descubre nuevo talento en los escritores de Entre Lineas y los conecta a compañías globales de multimedia.</p>
+                    <p className='thirdTextRightSecond'>Entre Lineas Studios trabaja con asociados tales como:</p>
+                    <div className='svgContainer'>
+                        <div className='sony'></div>
+                        <div className='hulu'></div>
+                        <div className='syfy'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default SecondSection
