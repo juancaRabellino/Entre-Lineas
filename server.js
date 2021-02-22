591f88bb0e00fabb9201602569e31bf15b8d818c
Santiago Alessandro <saantialessa@gmail.com>
2021-02-22 04:35:30 -0300

    finaly deploy

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.use(fileUpload())
 //Ruteo
 app.use('/api', router)
 
-// if(process.env.NODE_ENV === 'production'){
-//   app.use(express.static('client/build'))
-//   app.get('*', (req, res)=>{
-//     res.sendFile(path.join(__dirname+"/client/build/index.html"))
-//   })
-// }
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+  app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname+"/client/build/index.html"))
+  })
+}
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0'
