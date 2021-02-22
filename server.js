18e70d833be4c53da8a22c9afe07b217df4c4624
Santiago Alessandro <saantialessa@gmail.com>
2021-02-22 03:07:53 -0300

    first commit before deploy

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.use(fileUpload())
 //Ruteo
 app.use('/api', router)
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
-  app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname+"/client/build/index.html"))
-  })
-}
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static('client/build'))
+//   app.get('*', (req, res)=>{
+//     res.sendFile(path.join(__dirname+"/client/build/index.html"))
+//   })
+// }
 
 const port = process.env.PORT
 const host = process.env.HOST || '0.0.0.0'
