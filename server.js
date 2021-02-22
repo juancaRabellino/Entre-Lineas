657f11fa850ad6e81d70e162fc6f470bd80599e3
Santiago Alessandro <saantialessa@gmail.com>
2021-02-22 03:38:32 -0300

    a

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api', router)
 //   })
 // }
 
-const port = process.env.PORT
-const host = process.env.HOST || '0.0.0.0'
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || '0.0.0.0'
 
-app.listen(port, host, ()=> console.log('App listening on port 4000'))
+app.listen(PORT, HOST, ()=> console.log('App listening on port' + port ))
