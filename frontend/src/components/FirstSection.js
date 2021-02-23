252dd674b35e8cfbc517490843099b56c241ca6d
Santiago Alessandro <saantialessa@gmail.com>
2021-02-23 08:44:32 -0300

    upgrades

--- frontend/src/components/FirstSection.js
+++ frontend/src/components/FirstSection.js
@@ -8,8 +8,8 @@ const FirstSection = ()=>{
                 <p className="firstP">La plataforma del mundo social basada en narrativa más amada en el mundo</p>
                 <p className="secondP">Entre Lineas conecta una comunidad global de 90 millones de lectores y escritores a través del poder de las historias.</p>
                 <div className="buttonBox">
-                    <Link to='/search'><button className="btn btn-danger">Comienza a leer</button></Link>
-                    <Link to='/signin'><button className="btn btn-danger boton2" >Comienza a escribir</button></Link>
+                <Link to='/search'><button className="btn btn-danger">Comienza a leer</button></Link>
+                <Link to='/signin'><button className="btn btn-danger boton2" >Comienza a escribir</button></Link>
                 </div>
             </div>
             <div className="rightBoxFirst"></div>
